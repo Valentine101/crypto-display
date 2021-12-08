8862057266ee7b23c85e793ee1c9b29ce5721524
Valentine101 <74081251+Valentine101@users.noreply.github.com>
2021-12-08 18:21:51 -0500

    background gradient added

---
+++ crypto-display/src/CryptoGraph.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class CryptoGraph extends Component {
+    render() {
+        return(
+            <p>Hello World</p>
+        )
+    }
+}
