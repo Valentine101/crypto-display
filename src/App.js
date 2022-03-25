e802b62319f3c1145adb03051fb139831af52dc7
Valentine101 <valentin3sch@gmail.com>
2022-03-24 22:37:56 -0400

    Loading Symbols Added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
 import CryptoGraph from './CryptoGraph.js'
+import { SpinningCircles } from 'react-loading-icons'
 
 function App() {
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch("https://min-api.cryptocompare.com/data/generateAvg?fsym=ETH&tsym=USD&e=Coinbase&api_key=95b514849a7f4a7ccfd570eae8ae950a4fbb00e0ef2daa452e25521282163fc5")
+
+    const interval = setInterval( async() => {
+      fetch("https://min-api.cryptocompare.com/data/generateAvg?fsym=ETH&tsym=USD&e=Coinbase&api_key=95b514849a7f4a7ccfd570eae8ae950a4fbb00e0ef2daa452e25521282163fc5")
         .then(res => {
           return res.json()
         })
         .then(data => {
           //console.log(data)
           setList(data.DISPLAY)
+          console.log(data.DISPLAY.CHANGEPCT24HOUR)
         })
+    }, 10000)
+
+    return () => { clearInterval(interval) }
   }, [])
 
   //console.log(list)
@@ -21,7 +28,7 @@ function App() {
   return (
     <div className="App">
       <CryptoGraph />
-      <p>Ethereum has changed {list.CHANGEPCT24HOUR} % the last 24hrs</p>
+      {(list.CHANGEPCT24HOUR) ? `Ethereum has changed ${list.CHANGEPCT24HOUR} % the last 24hrs` : <SpinningCircles />}
     </div>
   )
 }
